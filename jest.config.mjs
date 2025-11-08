782add40fb86910944346f753e418a664518426a
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-11-08 21:01:14 +0900

    chore: alias 추가 및 Jest 매핑 보완
    - Jest moduleNameMapper를 보완해 .js 경로를 .ts로 매핑

--- jest.config.mjs
+++ jest.config.mjs
@@ -12,6 +12,10 @@ export default {
   },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@core/(.*)\\.js$': '<rootDir>/src/core/$1.ts',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@test/(.*)\\.js$': '<rootDir>/src/__test__/$1.ts',
+    '^@test/(.*)$': '<rootDir>/src/__test__/$1',
   },
   testMatch: ['**/?(*.)+(spec|test).ts'],
   collectCoverageFrom: ['src/**/*.ts'],
